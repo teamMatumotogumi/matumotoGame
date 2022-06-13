c2f9314254fd2e562d4d6fab321ac7da59cfd3f1
Kaitobt <mtgvangrd834@gmail.com>
2022-06-13 18:42:51 +0900

    Audio
    Audio.hの完成
    後はcppを作ってゲームに実装するだけ

--- Bチーム作品真/DirectX12プログラミング/DirectX12プログラミング/Audio.h
+++ Bチーム作品真/DirectX12プログラミング/DirectX12プログラミング/Audio.h
@@ -1,15 +1,15 @@
 #pragma once
+#include<Windows.h>
 
-class Audio
-{
-public:
+// 音楽ファイルを開く
+// パスで識別
+WORD OpenAudio(const char* path);
 
+// 音楽を開く
+void PlayAudio(WORD deviceID);
 
+// 音楽を一時停止する
+void StopAudio(WORD deviceID);
 
-private:
-
-
-
-};
-
-
+// 音楽を閉じる
+void closeAudio(WORD deviceID);
